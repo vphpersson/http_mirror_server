12527dcb22522808c6187de2713a7027d8cc987d
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2022-07-09 19:48:10 +0200

    Add missing `setup.py`.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='http_mirror_server',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'typed_argument_parser @ git+https://github.com/vphpersson/typed_argument_parser.git#egg=typed_argument_parser',
+        'public_suffix @ git+https://github.com/vphpersson/public_suffix.git#egg=public_suffix',
+        'ecs_py @ git+https://github.com/vphpersson/ecs_py.git#egg=ecs_py',
+        'ecs_tools_py @ git+https://github.com/vphpersson/ecs_tools_py.git#egg=ecs_tools_py'
+    ]
+)
